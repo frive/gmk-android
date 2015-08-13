98b0a658fdcc9c517e5310d9c2ae9ebaca370017
frive <felmrive@gmail.com>
2015-08-13 15:00:51 +0800

    Place detail activity ui improvements, fixed xml tabs.

--- GmkAndroid/src/main/java/com/gmk/gmkandroid/ui/activity/PlaceActivity.java
+++ GmkAndroid/src/main/java/com/gmk/gmkandroid/ui/activity/PlaceActivity.java
@@ -1,6 +1,7 @@
 package com.gmk.gmkandroid.ui.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -8,8 +9,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import net.steamcrafted.materialiconlib.MaterialDrawableBuilder;
@@ -22,6 +25,12 @@ public class PlaceActivity extends BaseActivity {
 
   @Bind(R.id.ivPlaceHeader) ImageView ivPlaceHeader;
   @Bind(R.id.fabMapMarker) FloatingActionButton fabMapMarker;
+  @Bind(R.id.tvFaves) TextView tvFaves;
+  @Bind(R.id.tvCuisine) TextView tvCuisine;
+  @Bind(R.id.tvAddress) TextView tvAddress;
+  @Bind(R.id.tvPhone) TextView tvPhone;
+  @Bind(R.id.tvWeb) TextView tvWeb;
+  @Bind(R.id.tvEmail) TextView tvEmail;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -36,21 +45,75 @@ public class PlaceActivity extends BaseActivity {
     setSupportActionBar(toolbar);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    int colorTitle = getResources().getColor(R.color.white);
+
     CollapsingToolbarLayout collapsingToolbar =
         (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
     collapsingToolbar.setTitle(place.getName());
+    collapsingToolbar.setCollapsedTitleTextColor(colorTitle);
+    collapsingToolbar.setExpandedTitleColor(colorTitle);
 
     loadBackdrop();
   }
 
   private void loadBackdrop() {
+    int colorPrimary = getResources().getColor(R.color.primary);
+
     Drawable imgMapMarker = MaterialDrawableBuilder.with(this)
         .setIcon(MaterialDrawableBuilder.IconValue.MAP_MARKER)
         .setColor(getResources().getColor(R.color.white))
         .build();
 
+    Drawable imgFave = MaterialDrawableBuilder.with(this)
+        .setIcon(MaterialDrawableBuilder.IconValue.HEART)
+        .setColor(colorPrimary)
+        .build();
+
+    Drawable imgCuisine = MaterialDrawableBuilder.with(this)
+        .setIcon(MaterialDrawableBuilder.IconValue.SILVERWARE_VARIANT)
+        .setColor(colorPrimary)
+        .build();
+
+    Drawable imgAddress = MaterialDrawableBuilder.with(this)
+        .setIcon(MaterialDrawableBuilder.IconValue.HOME)
+        .setColor(colorPrimary)
+        .build();
+
+    Drawable imgPhone = MaterialDrawableBuilder.with(this)
+        .setIcon(MaterialDrawableBuilder.IconValue.PHONE)
+        .setColor(colorPrimary)
+        .build();
+
+    Drawable imgEmail = MaterialDrawableBuilder.with(this)
+        .setIcon(MaterialDrawableBuilder.IconValue.EMAIL)
+        .setColor(colorPrimary)
+        .build();
+
+    Drawable imgWeb = MaterialDrawableBuilder.with(this)
+        .setIcon(MaterialDrawableBuilder.IconValue.WEB)
+        .setColor(colorPrimary)
+        .build();
+
     //Glide.with(this).load(Cheeses.getRandomCheeseDrawable()).centerCrop().into(imageView);
     fabMapMarker.setImageDrawable(imgMapMarker);
+
+    tvFaves.setText("100");
+    tvFaves.setCompoundDrawables(imgFave, null, null, null);
+
+    tvCuisine.setText(place.getCuisine());
+    tvCuisine.setCompoundDrawables(imgCuisine, null, null, null);
+
+    tvAddress.setText(place.getAddress());
+    tvAddress.setCompoundDrawables(imgAddress, null, null, null);
+
+    tvPhone.setText(place.getPhone());
+    tvPhone.setCompoundDrawables(imgPhone, null, null, null);
+
+    tvEmail.setText(place.getEmail());
+    tvEmail.setCompoundDrawables(imgEmail, null, null, null);
+
+    tvWeb.setText(place.getWebsite());
+    tvWeb.setCompoundDrawables(imgWeb, null, null, null);
   }
 
   @Override
